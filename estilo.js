58fbc5316c13f64e3319229d586a66d046710a23
Npedemonte <nicolas.pedemonte.1.1@gmail.com>
2022-10-18 13:27:46 -0300

    first commit

---
+++ estilo.js
@@ -0,0 +1,12 @@
+let video = document.querySelectorAll("video")
+
+
+const reproducir=()=>{
+	console.log("click")
+	video[0].play()
+}
+
+const pausar=()=>{
+console.log("clikeo pause")
+video[0].pause()
+}
